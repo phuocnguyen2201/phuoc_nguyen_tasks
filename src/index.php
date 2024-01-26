305c57e219e69ffca2384d3215d8a7a6e96a8474
phuocng <phuocnguyen2201@gmail.com>
2024-01-26 22:05:05 +0200

    Update index page

--- src/index.php
+++ src/index.php
@@ -1,8 +1,3 @@
 <?php
-$color = "varible";
-$main_theme= "#FDC822";
-echo "<h1>hello world this is Phuoc Ng testing</h1>";
-echo "<h2>This is a ". $color."</h2>";
-echo "this is my main theme color: ".$main_theme;
-echo "<div style=background-color:".$main_theme."> this is a div with the color looks like   </div>"
+echo "Hello, World!";
 ?>
