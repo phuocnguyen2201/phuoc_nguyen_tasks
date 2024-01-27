b37124645e29b92a86ada6dbcd9f8795aa84f7b9
phuocng <phuocnguyen2201@gmail.com>
2024-01-27 02:14:38 +0200

    Update ex1

--- src/ex1.php
+++ src/ex1.php
@@ -4,6 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Exercise 1: Getting Started with PHP - Phuoc</title>
+    <style>
+        table, th, td {
+            border: 1px solid black;
+            border-collapse: collapse;
+        }
+        </style>
 </head>
 <body>
 
@@ -18,12 +24,29 @@
         $g2 = 4;
         $g3 = 5;
 
-        echo "<table style='border: 1px solid black;'><tr><th>S.n.</th><th>Name</th><th>Grade</th></tr>";
-        echo "<tr><th>1</th><th>John</th><th>".$g1."</th></tr>";
-        echo "<tr><th>2</th><th>Alice</th><th>".$g2."</th></tr>";
-        echo "<tr><th>3</th><th>Bob</th><th>".$g3."</th></tr></table>";
-
-        echo "<img alt=\"Env development confirm\" src=\"./image/env_phuoc_nguyen.PNG\">";
         ?>
+          <table>
+            <tr>
+                <th>S.n.</th>
+                <th>Name</th>
+                <th>Grade</th>
+            </tr>
+            <tr>
+                <th>1</th>
+                <th>John</th>
+                <th><?php echo $g1; ?></th>
+            </tr>
+            <tr>
+                <th>2</th>
+                <th>Alice</th>
+                <th><?php echo $g2; ?></th>
+            </tr>
+            <tr>
+                <th>3</th>
+                <th>Bob</th>
+                <th><?php echo $g3; ?></th>
+            </tr>
+            </table>
+        <?php echo "<img alt=\"Env development confirm\" src=\"./image/env_phuoc_nguyen.PNG\">";?>
 </body>
 </html>
