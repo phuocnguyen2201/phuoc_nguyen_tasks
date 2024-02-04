3e3b3d797e2410190a1618a8aa895ebcf02d356a
phuocng <phuocnguyen2201@gmail.com>
2024-02-04 18:38:12 +0200

    change error page path

--- src/individual_tasks/index.php
+++ src/individual_tasks/index.php
@@ -1,3 +1,15 @@
 <?php
     session_start();
-?>
+    include "connection.php";
+    include "query.php";
+    
+    if(isset($_POST['form-login'])){
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        
+        $match = connect_and_query(query_command::query_account($username, $password));
+        
+        if ($match->num_rows > 0 && !isset($_COOKIE['username'])) {
+            setcookie("username", $username, time() + 10, "/");
+        }
+    }
